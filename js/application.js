02514378288446a8ca6226d59fab47541c4e0d79
Nick AMancini <nmancini11@gmail.com>
2014-09-07 19:28:19 -0700

    fixed multiple player issue. All players will start regardless of which audio player was clicked. This could have been a browser caching issue..

--- js/application.js
+++ js/application.js
@@ -5,16 +5,16 @@ $(document).ready(function () {
     function loadSongs(){
         var x = 0;
         $('.audio-song').each(function(){
-           var m4a = $(this).find(".jp-audio").data( "audio-ma" ),
+            x++;
+            var m4a = $(this).find(".jp-audio").data( "audio-ma" ),
                ogg = $(this).find(".jp-audio").data( "audio-ogg" );
 
-           x++;
-
-           $(this).find(".jp-jplayer").attr('id', 'jquery_jplayer_'+x);
-           $(this).find(".jp-audio").attr('id', 'jp_container_'+x);
-           // $(this).find(".jp-gui").addClass("jp-interface_"+x);
-           js_audioPlayer(m4a, ogg, x);
-           console.log(m4a +"  "+ogg +"  "+ x);
+            $(this).find(".jp-jplayer").attr('id', 'jquery_jplayer_'+x);
+            // $(this).find(".jp-audio").attr('id', 'jp_container_'+x);
+            $(this).find(".jp-audio").attr('id', 'jp_interface_'+x);
+            // callback js_audioPlayer to create
+            js_audioPlayer(m4a, ogg, x);
+            // console.log(m4a +"  "+ogg +"  "+ x);
         });
     }
 
@@ -28,7 +28,7 @@ $(document).ready(function () {
                     ogg: fileOgg
               });
             },
-            cssSelectorAncestor: "#jp_container_" + location,
+            cssSelectorAncestor: "#jp_interface_" + location,
             swfPath: "/swf",
             wmode: "window",
             supplied: "m4a, oga",
@@ -37,6 +37,7 @@ $(document).ready(function () {
             remainingDuration: true,
             toggleDuration: true
           });
+
         // pause all other players
         $("#jquery_jplayer_" + location).bind($.jPlayer.event.play, function() {
           $(this).jPlayer("pauseOthers"); // pause all players except this one.
@@ -44,29 +45,22 @@ $(document).ready(function () {
     return;
     }
 
-    function stopAll(){
-        $(".jplyrselectr").jPlayer("stop");
-        console.log("Aye aye captain! Pausing all");
-    }
-
-    $('.jp-play').on("click", stopAll);
-
 
     //open-close submenu on mobile
     $('.cd-main-nav').on('click', function() {
-    	if (!$('.cd-main-nav input').is(':focus') && $(window).width() < 768) {
-			$(this).children('ul').toggleClass('is-visible short-animated slideInDown');
-		}
+        if (!$('.cd-main-nav input').is(':focus') && $(window).width() < 768) {
+            $(this).children('ul').toggleClass('is-visible short-animated slideInDown');
+        }
     });
 
     $(window).on('scroll', function () {
-    	var $headerHeight = 100;
-    	if ($(window).width() < 768){
-    		$headerHeight = 45;
-    	}
-    	else if ($(window).width() < 960){
-    		$headerHeight = 70;
-    	}
+        var $headerHeight = 100;
+        if ($(window).width() < 768){
+            $headerHeight = 45;
+        }
+        else if ($(window).width() < 960){
+            $headerHeight = 70;
+        }
 
         $('.jp-audio').each(function () {
             if ($(this).offset().top <= $(window).scrollTop() + $headerHeight) {
@@ -79,7 +73,7 @@ $(document).ready(function () {
     });
 
     $('.spin').mouseenter(function(){
-    	$(this).addClass("animated rotateIn");
+        $(this).addClass("animated rotateIn");
     });
     
 });
